957eef9b811dbf6628f42922960f6bb157fe89e5
Edward Wong <wong.eddiek@gmail.com>
2017-07-23 15:19:44 -0600

    Solved up to problem 8

--- scripts/min/euler-min.js
+++ scripts/min/euler-min.js
@@ -293,7 +293,7 @@ function smallestMultiple(max) {
   return sum;
 }
 
-//Sum Square Difference.  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+//Problem 6 - Sum Square Difference.  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 function sumOfSquared(max) {
   var sum = 0;
   for (var i = 1; i <= max; i++) {
@@ -313,6 +313,66 @@ function squaredOfSum(max) {
 
 function squaredSumDiff(max) {
   return squaredOfSum(max) - sumOfSquared(max);
+}
+
+//Problem 7 - 1001st prime
+//prime finder.
+function isPrime(num){
+  if (num != 2 && num % 2 === 0) return false;
+  for (var i = 3; i <= Math.sqrt(num); i += 2) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//generates a prime list from 2 - max
+function primeList(max) {
+  var primelist = []
+  for (var i = 2; i <= max; i++) {
+    if (isPrime(i)) {
+      primelist.push(i);
+    }
+  }
+  return primelist;
+}
+
+//generates a prime list containing n number of primes.
+function nPrimeList(n) {
+  var primelist = []
+  for (var i = 2; primelist.length < n; i++) {
+    if (isPrime(i)) {
+      primelist.push(i);
+    }
+  }
+  return primelist;
+}
+
+//return the nth prime starting at 2.
+function findNthPrime(n) {
+  return nPrimeList(n).pop();
+}
+
+//Problem 8 - Largest product in a series.
+function largestProduct() {
+  var series = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+
+  var productArr = []
+  for (var i = 0; i < series.length; i++) {
+    var product = 1;
+    for (var j = i; j < i+13; j++) {
+      product = product * series[j]
+    }
+    productArr.push(product);
+  }
+  var largestNum = -Infinity
+  for (var i = 0; i < productArr.length; i++) {
+    if (productArr[i] > largestNum) {
+      largestNum = productArr[i];
+    }
+  }
+  return largestNum;
 }
 
 
